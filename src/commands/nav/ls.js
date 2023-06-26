2d2e83cc4da9091f7a577a450cdfe87c6e40d1b4
ITboo <annaacdc@tut.by>
2023-06-26 20:56:01 +0300

    refactor: update ls

--- src/commands/nav/ls.js
+++ src/commands/nav/ls.js
@@ -1,12 +1,37 @@
 import { readdir } from 'fs/promises';
 import { resolve } from 'path';
 
-export const ls = async() => {
-  try {
-    const currentDir = resolve(process.cwd());
-    const filesList = await readdir(currentDir);
-    console.table(filesList);
-  } catch (error) {
-    console.error('Operation failed');
+//TITLES
+const name_title = 'name';
+const type_title = 'type';
+//TYPES
+const file_value = 'file';
+const dir_value = 'directory';
+
+export const ls = async () => {
+  const currentDir = resolve(process.cwd());
+  const filesList = await readdir(currentDir, { withFileTypes: true });
+  const list = filesList
+    .filter(typeFilter)
+    .map(table)
+    .sort(typeSort);
+
+  console.table(list);
+};
+
+const typeFilter = (file) => {
+  return file.isDirectory() || file.isFile();
+};
+
+const table = (file) => {
+  return {
+    [name_title]: file.name,
+    [type_title]: file.isFile() ? file_value : dir_value,
   };
+};
+
+const typeSort = (a, b) => {
+  return a.type === b.type
+    ? a.name.localeCompare(b.name)
+    : a.type === file_value ? 1 : -1;
 };
